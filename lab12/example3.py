da0c5e84c8e9b818aeba5220aab27ea49e67a0ac
k23040198 <sudecevik@std.iyte.edu.tr>
2021-01-18 11:33:29 +0000

    DNA class

---
+++ lab12/example3.py
@@ -0,0 +1,34 @@
+class DNA():
+  def __init__(self,dna):
+    self.dna=dna
+    self.dna_dict={"A":0,"T":0,"G":0,"C":0}
+
+  def count_nucleotides(self):
+    for _ in self.dna:
+      if _== "A":
+        self.dna_dict["A"]+=1
+      elif _== "T":
+        self.dna_dict["T"]+=1
+      elif _== "G":
+        self.dna_dict["G"]+=1
+      else:
+        self.dna_dict["C"]+=1
+    return self.dna_dict
+
+  def calculate_complement(self):
+    new_dna=""
+    for _ in self.dna:
+      if _== "A":
+        new_dna+="T"
+      elif _== "T":
+        new_dna+="A"
+      elif _== "G":
+        new_dna+="C"
+      else:
+        new_dna+="G"
+
+    return new_dna
+
+d1=DNA("ATGC")
+print(d1.count_nucleotides())
+print(d1.calculate_complement())
---
+++ lab13/example1.py
@@ -0,0 +1,16 @@
+def binary_search(list_, element):
+    if len(list_) == 0:
+        return -1
+
+    else:
+        mid_element = len(list_) // 2
+        if element == list_[mid_element]:
+            return mid_element
+        elif element < list_[mid_element]:
+            return binary_search(list_[0:mid_element], element)
+        else:
+            return binary_search(list_[mid_element + 1:], element) + mid_element + 1
+
+
+print(binary_search([ 1,2, 5, 6, 8, 9, 74, 98, 100],3))
+
